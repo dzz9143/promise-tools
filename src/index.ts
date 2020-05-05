7d025134e801e5f8ce73d74bc4e07707dfdfde42
dzz9143 <zhizhuoding@qq.com>
2020-05-05 15:44:42 +0800

    export withCache, withTimeout & withRetry

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import np from './np';
 import parallel from './parallel';
 import sleep from './sleep';
+import withCache from './withCache';
+import withTimeout from './withTimeout';
+import withRetry from './withRetry';
 
-export { np, parallel, sleep };
+export { np, parallel, sleep, withCache, withTimeout, withRetry };
--- test/index.test.ts
+++
@@ -1,5 +0,0 @@
-describe('dump test', () => {
-    it('should 1 equals 1', () => {
-        expect(1).toEqual(1);
-    });
-});
